f3604c3b090fa6faa72f06a274cba8e797811977
James Halliday <mail@substack.net>
2012-02-13 22:54:57 -0800

    gamma function transliterated from python

---
+++ index.js
@@ -0,0 +1,35 @@
+// transliterated from the python snippet here:
+// http://en.wikipedia.org/wiki/Lanczos_approximation
+
+var g = 7;
+var p = [
+    0.99999999999980993,
+    676.5203681218851,
+    -1259.1392167224028,
+    771.32342877765313,
+    -176.61502916214059,
+    12.507343278686905,
+    -0.13857109526572012,
+    9.9843695780195716e-6,
+    1.5056327351493116e-7
+];
+
+module.exports = function gamma (z) {
+    if (z < 0.5) {
+        return Math.PI / Math.sin(Math.PI * z) * gamma(1 - z);
+    }
+    else {
+        z -= 1;
+        var x = p[0];
+        for (var i = 1; i < g + 2; i++) {
+            x += p[i] / (z + i);
+        }
+        var t = z + g + 0.5;
+        
+        return Math.sqrt(2 * Math.PI)
+            * Math.pow(t, z + 0.5)
+            * Math.exp(-t)
+            * x
+        ;
+    }
+};
